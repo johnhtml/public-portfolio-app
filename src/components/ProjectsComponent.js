f9b009cc3b833058951135f1f91232198b309b32
johnhtml <jogerenag@unal.edu.co>
2021-08-24 16:33:20 -0500

    Project component change 2

--- src/components/ProjectsComponent.js
+++ src/components/ProjectsComponent.js
@@ -4,7 +4,7 @@ import DraggableComponent from './DraggableComponent';
 function Projects() {
     return (
         <div>
-            <p>Interact with the posterior <em>draggable</em> list:</p>
+            <p>Interact with the posterior <em>"draggable"</em> list:</p>
             <DraggableComponent />
         </div>
     );
